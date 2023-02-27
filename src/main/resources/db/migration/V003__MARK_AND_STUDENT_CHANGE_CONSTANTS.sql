7a03d0e182a55007aa52d68db0b2807ac31c4be4
Degtiar Yaroslav <merrynan2001@gmail.com>
2023-02-28 00:04:14 +0300

    third flyway migration

---
+++ src/main/resources/db/migration/V003__MARK_AND_STUDENT_CHANGE_CONSTANTS.sql
@@ -0,0 +1,5 @@
+ALTER TABLE my_university.mark ALTER COLUMN status DROP NOT NULL;
+ALTER TABLE my_university.mark ALTER COLUMN discipline DROP NOT NULL;
+
+ALTER TABLE my_university.student ALTER COLUMN name DROP NOT NULL;
+ALTER TABLE my_university.student ALTER COLUMN email DROP NOT NULL;
