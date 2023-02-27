02de57d687d490488c144aad07c12bc9d3e2bfa1
Degtiar Yaroslav <merrynan2001@gmail.com>
2023-02-27 23:56:55 +0300

    initial commit

---
+++ src/test/java/com/example/javapro_hm_26_hibernate/JavaProHm26HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapro_hm_26_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProHm26HibernateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
